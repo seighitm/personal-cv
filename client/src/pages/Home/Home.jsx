0acba1ad62d12e03dbf5f138a5678ca8e0b97f1a
Mihail <mseighit@gmail.com>
2021-05-19 16:28:53 +0300

    Added social media button for home page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Social from "./Social";
 import './home.css'
 import profilePhoto from "../../assets/img/profilePhoto.png"
 import pdf from '../../assets/cv.pdf'
@@ -11,6 +12,7 @@ const Home = () => {
             </div>
             <div className="home__title">Seighit Mihail</div>
             <span className="home__profession">Web Developer</span>
+            <Social/>
             <a download="" href={pdf} className="button home__button">Download Resume</a>
         </div>
     );
