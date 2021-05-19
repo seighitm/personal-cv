a39f053d9c93fba02a15e650deeefb57382e412a
Mihail <mseighit@gmail.com>
2021-05-19 16:32:20 +0300

    Added route for home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
+import {Route, Switch} from 'react-router-dom'
+import Home from "./pages/Home/Home";
+import './App.css'
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className='infoScreen'>
+            <Switch>
+                <Route path='/' component={Home}/>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
