e6319a093c0598af008dd970a108ae68ecee1a1f
Mihail <mseighit@gmail.com>
2021-05-19 20:47:59 +0300

    Added buttons for navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
 import {Route, Switch} from 'react-router-dom'
 import Home from "./pages/Home/Home";
 import './App.css'
-import React from "react";
+import React, {useState} from "react";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
+    const itemsNavbar = ["about", "skills", "education", "services", "contact"]
+    const icons = ['bxs-user-circle', 'bxs-network-chart', 'bxs-book-bookmark', 'bxs-server', 'bxs-contact']
+    const [activeItem, setActiveItem] = useState(0)
+
+    const onSelectedCategory = React.useCallback((index) => {
+        setActiveItem(index)
+    }, []);
+
     return (
-        <div className='infoScreen'>
-            <Switch>
-                <Route path='/' component={Home}/>
-            </Switch>
+        <div>
+            <Navbar icons={icons} activeItem={activeItem} setActiveItem={onSelectedCategory} itemsNavbar={itemsNavbar}/>
+            <div className='infoScreen'>
+                <Switch>
+                    <Route path='/' component={Home}/>
+                </Switch>
+            </div>
+
         </div>
     );
 }
