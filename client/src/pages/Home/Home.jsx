7f1278fabeebb9a19e500478987acf5032624c30
Mihail <mseighit@gmail.com>
2021-05-19 16:25:52 +0300

    Created home page

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './home.css'
+import profilePhoto from "../../assets/img/profilePhoto.png"
+import pdf from '../../assets/cv.pdf'
+
+const Home = () => {
+    return (
+        <div className="home__data">
+            <div className="home__img">
+                <img src={profilePhoto} alt=""/>
+            </div>
+            <div className="home__title">Seighit Mihail</div>
+            <span className="home__profession">Web Developer</span>
+            <a download="" href={pdf} className="button home__button">Download Resume</a>
+        </div>
+    );
+};
+
+export default Home;
