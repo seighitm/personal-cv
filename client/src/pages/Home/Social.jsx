91eb52a757a6c042ce2df8093f051479aa468c83
Mihail <mseighit@gmail.com>
2021-05-19 16:28:53 +0300

    Added social media button for home page

---
+++ client/src/pages/Home/Social.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://www.linkedin.com/in/mihail-%C8%99-5a9947a9/" className="home__social-link">
+                <i className='bx bxl-linkedin'/></a>
+            <a href="https://github.com/seighitm" className="home__social-link">
+                <i className='bx bxl-github'/></a>
+            <a href="https://www.facebook.com/mihail.seighit/" className="home__social-link">
+                <i className='bx bxl-facebook-circle'/></a>
+            <a href="https://twitter.com/_mihai_s_" className="home__social-link">
+                <i className='bx bxl-twitter'/></a>
+        </div>
+    );
+};
+
+export default Social;
