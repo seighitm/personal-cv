cbc08cc75bf927ffb5b25bdb369365d7e52275e9
Mihail <mseighit@gmail.com>
2021-05-20 01:19:42 +0300

    Created page for skills

---
+++ client/src/pages/Skills/Skills.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Skills = () => {
+    return (
+        <section className="main skills_section section" id="skills">
+            <h2 className="section-title">My expertise area</h2>
+            <div className="skills__container bd-grid">
+                Skills
+            </div>
+        </section>
+    );
+};
+
+export default Skills;
