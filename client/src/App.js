280fe052b4d96b49197ce224ebd7aa8e25566d70
Mihail <mseighit@gmail.com>
2021-05-20 16:26:26 +0300

    Add route for contacts page

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,15 @@
 import {Route, Switch} from 'react-router-dom'
 import Home from "./pages/Home/Home";
-import './App.css'
-import React, {useState} from "react";
-import Navbar from "./components/Navbar/Navbar";
-import Education from "./pages/Education/Education";
 import AboutMe from "./pages/About/AboutMe";
 import Skills from "./pages/Skills/Skills";
+import Education from "./pages/Education/Education";
 import Services from "./pages/Services/Services";
-
+import Contact from "./pages/Contact/Contact";
+import Navbar from "./components/Navbar/Navbar";
+import './App.css'
+import React, {useState} from "react";
 function App() {
-    const itemsNavbar = ["about", "skills", "education", "services", "contact"]
+    const itemsNavbar = ["about", "skills", "education", "services","contact"]
     const icons = ['bxs-user-circle', 'bxs-network-chart', 'bxs-book-bookmark', 'bxs-server', 'bxs-contact']
     const [activeItem, setActiveItem] = useState(0)
 
@@ -24,14 +24,16 @@ function App() {
                 <Switch>
                     <Route path='/about' component={AboutMe} exact/>
                     <Route path='/skills' component={Skills} exact/>
-                    <Route path='/services' component={Services} exact/>
                     <Route path='/education' component={Education} exact/>
+                    <Route path='/services' component={Services} exact/>
+                    <Route path='/contact' component={Contact} exact/>
                     <Route path='/' component={Home}/>
                 </Switch>
             </div>
-
         </div>
     );
 }
+
+
 
 export default App;
