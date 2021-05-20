9547f760dcd06377d2108616eb0e40af4878db59
Mihail <mseighit@gmail.com>
2021-05-20 13:57:19 +0300

    Created services page

---
+++ client/src/pages/Services/Services.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <section className="services section" id="services">
+            <h2 className="section-title">My Services</h2>
+            <div className="services__container bd-grid">
+                Services
+            </div>
+        </section>
+    );
+};
+
+export default Services;
