8e78243930fde524a2ebccf6f7e3fbe10df9ff43
Mihail <mseighit@gmail.com>
2021-05-20 14:37:01 +0300

    Created contacts page

---
+++ client/src/pages/Contact/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <section className="contact__container bd-grid section" id="contact">
+            <div className='contactFull'>
+                Contacts
+            </div>
+        </section>
+    );
+};
+
+export default Contact;
